0d559d3422416431f026b5e132ae5d71e43f858a
Maksim Koshman <maksimkoshman090@gmail.com>
2020-12-12 20:20:50 +0300

    refactoring map.js

--- js/map.js
+++ js/map.js
@@ -1,22 +1,3 @@
-const types = ["palace", "flat", "house", "bungalo"];
-const rooms = [1, 2, 3, 4, 5];
-const checkTime = ["12:00", "13:00", "14:00"];
-const features = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioner",
-];
-const photos = [
-  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel3.jpg",
-];
-
-const map = document.querySelector(".map");
-
 // Возращает случайное число в диапазоне
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -42,250 +23,179 @@ function shuffle(array) {
   return newArray.sort(() => Math.random() - 0.5);
 }
 
-console.log(
-  randomLengthFeatures(features, getRandomIntInclusive(1, features.length))
-);
+let numbers = shuffle([1, 2, 3, 4, 5, 6, 7, 8]);
 
-// Данные для объявлений
-let adsNearby = [
-  {
-    author: {
-      avatar: "img/avatars/user01.png",
-    },
-    offer: {
-      title: "Большая уютная квартира",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user02.png",
-    },
-    offer: {
-      title: "Маленькая неуютная квартира",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user03.png",
-    },
-    offer: {
-      title: "Огромный прекрасный дворец",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user04.png",
-    },
-    offer: {
-      title: "Маленький ужасный дворец",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user05.png",
-    },
-    offer: {
-      title: "Красивый гостевой домик",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user06.png",
-    },
-    offer: {
-      title: "Некрасивый негостеприимный домик",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user07.png",
-    },
-    offer: {
-      title: "Уютное бунгало далеко от моря",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
-
-  {
-    author: {
-      avatar: "img/avatars/user08.png",
-    },
-    offer: {
-      title: "Неуютное бунгало по колено в воде",
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandomIntInclusive(1000, 1000000),
-      type: types[getRandomIntInclusive(0, types.length - 1)],
-      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
-      guests: getRandomIntInclusive(1, 10),
-      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
-      features: randomLengthFeatures(),
-      description: "",
-      photos: shuffle(photos),
-    },
-    location: {
-      x: getRandomIntInclusive(40, map.offsetWidth - 40),
-      y: getRandomIntInclusive(130, 630),
-    },
-  },
+const titles = [
+  "Большая уютная квартира",
+  "Маленькая неуютная квартира",
+  "Огромный прекрасный дворец",
+  "Маленький ужасный дворец",
+  "Красивый гостевой домик",
+  "Некрасивый негостеприимный домик",
+  "Уютное бунгало далеко от моря",
+  "Неуютное бунгало по колено в воде",
 ];
 
-// Создание меток на карте
-let mapPins = document.querySelector(".map__pins");
+const types = ["palace", "flat", "house", "bungalo"];
+const rooms = [1, 2, 3, 4, 5];
+const checkTime = ["12:00", "13:00", "14:00"];
+const features = [
+  "wifi",
+  "dishwasher",
+  "parking",
+  "washer",
+  "elevator",
+  "conditioner",
+];
+
+const photos = shuffle([
+  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+  "http://o0.github.io/assets/images/tokyo/hotel3.jpg",
+]);
+
+// Создание новых объявлений
+const map = document.querySelector(".map");
+map.classList.remove("map--faded");
+
+let pins = [];
+
+function createPin(i) {
+  return {
+    author: {
+      avatar: "img/avatars/user0" + numbers[i] + ".png",
+    },
+    offer: {
+      title: titles[numbers[i] - 1],
+      address: `${getRandomIntInclusive(100, 1000)}, ${getRandomIntInclusive(
+        100,
+        1000
+      )}`,
+      price: getRandomIntInclusive(1000, 1000000),
+      type: types[getRandomIntInclusive(0, types.length - 1)],
+      rooms: rooms[getRandomIntInclusive(0, rooms.length - 1)],
+      guests: getRandomIntInclusive(1, 10),
+      checkin: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
+      checkout: checkTime[getRandomIntInclusive(0, checkTime.length - 1)],
+      features: randomLengthFeatures(),
+      description: "",
+      photos: photos,
+    },
+    location: {
+      x: getRandomIntInclusive(40, map.offsetWidth - 40),
+      y: getRandomIntInclusive(130, 630),
+    },
+  };
+}
+
+for (let i = 0; i < 8; i++) {
+  pins.push(createPin(i));
+}
+
+// Вывод новых меток
+const mapPins = document.querySelector(".map__pins");
+const mapPinTemplate = document
+  .querySelector("template")
+  .content.querySelector(".map__pin");
+
+function renderPin(mapPin) {
+  let mapPinElement = mapPinTemplate.cloneNode(true);
+
+  mapPinElement.querySelector("img").src = mapPin.author.avatar;
+  mapPinElement.querySelector("img").alt = mapPin.offer.title;
+  mapPinElement.style.left = mapPin.location.x + "px";
+  mapPinElement.style.top = mapPin.location.y + "px";
+
+  return mapPinElement;
+}
+
 let fragment = document.createDocumentFragment();
-
-for (let i = 0; i < adsNearby.length; i++) {
-  // Показ первого оьъявления
-  if (i === 0) {
-    let newAds = document.createElement("article");
-    newAds.className = "map__card";
-    newAds.innerHTML = `
-    <img src="${adsNearby[i].author.avatar}" class="popup__avatar" width="70" height="70">
-    <button class="popup__close">Закрыть</button>
-    <h3>${adsNearby[i].offer.title}</h3>
-    <p><small>${adsNearby[i].offer.address}</small></p>
-    <p class="popup__price">${adsNearby[i].offer.price}&#x20bd;/ночь</p>
-    <h4>${adsNearby[i].offer.type}</h4>
-    <p>${adsNearby[i].offer.rooms} комнаты для ${adsNearby[i].offer.guests} гостей</p>
-    <p>Заезд после ${adsNearby[i].offer.checkin}, выезд до ${adsNearby[i].offer.checkout}</p>
-    <ul class="popup__features">
-
-    </ul>
-    <p>${adsNearby[i].offer.description}</p>
-    <ul class="popup__pictures">
-    </ul>
-    `;
-    map.appendChild(newAds);
-
-    console.log(adsNearby[0].offer.features);
-    // Вывод удобств
-    for (let j = 0; j < adsNearby[i].offer.features.length; j++) {
-      console.log(adsNearby[j].offer.features);
-      let popupFeatures = document.querySelector(".popup__features");
-      let feature = document.createElement("li");
-      feature.className = `feature feature--${features[j]}`;
-      popupFeatures.appendChild(feature);
-    }
-
-    // Вывод фотографий
-    for (let g = 0; g < adsNearby[i].offer.photos.length; g++) {
-      let popupPictures = document.querySelector(".popup__pictures");
-      let picture = document.createElement("li");
-      picture.innerHTML = `<img width="50px" src="${adsNearby[i].offer.photos[g]}">`;
-      popupPictures.appendChild(picture);
-    }
-  }
-
-  let newPin = document.createElement("button");
-  newPin.className = "map__pin";
-  newPin.style.cssText = `left: ${adsNearby[i].location.x}px; top: ${adsNearby[i].location.y}px`;
-  newPin.innerHTML = `<img src="${adsNearby[i].author.avatar}" alt="${adsNearby[i].offer.title}" width="40" height="40"
-  draggable="false">`;
-
-  fragment.appendChild(newPin);
+for (let i = 0; i < pins.length; i++) {
+  fragment.appendChild(renderPin(pins[i]));
 }
 
 mapPins.appendChild(fragment);
+
+// Вывод первого объявления
+const mapCardTemplate = document
+  .querySelector("template")
+  .content.querySelector(".map__card");
+
+function getType(type) {
+  switch (type) {
+    case "flat":
+      return "Квартира";
+
+    case "bungalo":
+      return "Бунгало";
+
+    case "house":
+      return "Дом";
+
+    case "palace":
+      return "Дворец";
+
+    default:
+      return "";
+  }
+}
+
+// Выводит фотографии
+function printPhotos(mapCardElement, pin) {
+  let mapPhoto = mapCardElement.querySelector(".popup__photos li");
+
+  for (let i = 0; i < photos.length; i++) {
+    let mapPhotoElement = mapPhoto.cloneNode(true);
+    mapPhotoElement.querySelector("img").src = pin.offer.photos[i];
+
+    fragment.appendChild(mapPhotoElement);
+  }
+
+  mapPhoto.remove();
+
+  mapCardElement.querySelector(".popup__photos").appendChild(fragment);
+}
+
+// Вывод приемуществ
+function printFeatures(mapCardElement, pin) {
+  const mapFeaturesList = mapCardElement.querySelector(".popup__features");
+
+  for (let i = 0; i < pin.offer.features.length; i++) {
+    let mapFeature = document.createElement("li");
+    mapFeature.classList.add("feature");
+    mapFeature.classList.add("feature--" + pin.offer.features[i]);
+    fragment.appendChild(mapFeature);
+  }
+
+  mapFeaturesList.appendChild(fragment);
+}
+
+function createCard(pin) {
+  let mapCardElement = mapCardTemplate.cloneNode(true);
+  mapCardElement.querySelector(".popup__title").textContent = pin.offer.title;
+  mapCardElement.querySelector(".popup__text--address").textContent =
+    pin.offer.address;
+  mapCardElement.querySelector(".popup__text--price").textContent =
+    pin.offer.price + " ₽/ночь";
+  mapCardElement.querySelector(".popup__type").textContent = getType(
+    pin.offer.type
+  );
+  mapCardElement.querySelector(".popup__text--capacity").textContent =
+    pin.offer.rooms + "комнаты для" + pin.offer.guests + "гостей";
+  mapCardElement.querySelector(".popup__text--time").textContent =
+    "Заезд после" + pin.offer.checkin + ", выезд до" + pin.offer.checkout;
+
+  mapCardElement.querySelector(".popup__description").textContent =
+    pin.offer.description;
+
+  printFeatures(mapCardElement, pin);
+
+  printPhotos(mapCardElement, pin);
+
+  mapCardElement.querySelector(".popup__avatar").src = pin.author.avatar;
+
+  return mapCardElement;
+}
+
+fragment.appendChild(createCard(pins[0]));
+map.appendChild(fragment);
